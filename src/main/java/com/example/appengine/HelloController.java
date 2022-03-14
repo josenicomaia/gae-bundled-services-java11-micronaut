527035229dcb24a93adb525607da8459ea4fc240
José Nicodemos Maia Neto <jose@nicomaia.com.br>
2022-03-14 01:50:24 -0300

    Testando Datastore

--- src/main/java/com/example/appengine/HelloController.java
+++ src/main/java/com/example/appengine/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.appengine;
 
+import com.google.appengine.api.datastore.*;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
@@ -7,9 +8,28 @@ import io.micronaut.http.annotation.Produces;
 
 @Controller("/")
 public class HelloController {
-  @Get("/")
-  @Produces(MediaType.TEXT_PLAIN)
-  public String index() {
-    return "Hello World!";
-  }
+    private final DatastoreService ds;
+
+    public HelloController() {
+        ds = DatastoreServiceFactory.getDatastoreService();
+    }
+
+    @Get("/")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String index() {
+        Entity greeting = new Entity("Greeting");
+        greeting.setProperty("content", "Hello World! 321");
+
+        Key key = ds.put(greeting);
+
+        try {
+            if (ds.get(key) != null) {
+                return (String) ds.get(key).getProperty("content");
+            }
+
+            return "PQP2";
+        } catch (EntityNotFoundException e) {
+            return "PQP";
+        }
+    }
 }
